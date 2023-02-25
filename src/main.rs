b90da00e662f65059ea54f2b5fe2861835469e87
Chris Tozer <chris.tozer@metaswitch.com>
2023-02-25 21:49:42 +0000

    Now parsing 2 keywords, identifiers, and brackets

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+// Following https://norasandler.com/2017/11/29/Write-a-Compiler.html
+
 use anyhow::{bail, Result};
-use nom::character::complete::{digit1, multispace1};
+use nom::character::complete::{alphanumeric0, digit1, multispace1};
+use std::cmp::min;
 // use nom::IResult;
 
 #[derive(Debug)]
 pub enum CToken<'a> {
-    Unknown(&'a str), // Something we haven't yet identified
-    Keyword(&'a str), // A keyword like 'int' or 'return'
-    OpenParen,        // (
-    CloseParen,       // )
-    OpenBrace,        // {
-    CloseBrace,       // }
-    SemiColon,        // ;
+    // Keywords and identifiers
+    Keyword(&'a str), // A keyword like 'int' or 'return'.  TODO these should be an enum of their own, not a string.
+    Identifier(&'a str), // Variable, function, parameter name etc.
+    // Syntax
+    OpenParen,  // (
+    CloseParen, // )
+    OpenBrace,  // {
+    CloseBrace, // }
+    SemiColon,  // ;
     // Constants
     Integer(i32), // A number
     // Operators
@@ -20,7 +25,7 @@ pub enum CToken<'a> {
 
 // Entrypoint
 fn main() {
-    let input = String::from("   123 23 4  3");
+    let input = String::from("int main() {\n  return 2;\n}");
     println!("{:?}", lex(&input));
 }
 
@@ -32,7 +37,11 @@ fn lex(data: &String) -> Result<Vec<CToken>> {
 
     while data_slice.len() > 0 {
         let (token, bytes) = next_token(data_slice)?;
-        tokens.push(token);
+        if let CToken::Whitespace = token {
+            // Don't bother storing whitespace
+        } else {
+            tokens.push(token);
+        }
         bytes_read += bytes;
         data_slice = &data[bytes_read..]
     }
@@ -45,7 +54,13 @@ fn next_token(data: &str) -> Result<(CToken, usize)> {
         Some(ch) => match ch {
             ch if ch.is_digit(10) => int_token_from_slice(data),
             ch if ch.is_whitespace() => slurp_whitespace(data),
-            _ => Ok((CToken::Unknown(data), data.len())),
+            '{' => Ok((CToken::OpenBrace, 1)),
+            '}' => Ok((CToken::CloseBrace, 1)),
+            '(' => Ok((CToken::OpenParen, 1)),
+            ')' => Ok((CToken::CloseParen, 1)),
+            ';' => Ok((CToken::SemiColon, 1)),
+            // Anything left (at this point, should be a keyword or identifier - we don't handle commas etc. yet)
+            _ => keyword_or_identifier_from_slice(data),
         },
     }
 }
@@ -54,6 +69,35 @@ fn next_token(data: &str) -> Result<(CToken, usize)> {
 fn int_token_from_slice(s: &str) -> Result<(CToken, usize)> {
     let (_, int_slice) = digit1::<&str, ()>(s)?; // Ignoring error type (second parameterized type)
     Ok((CToken::Integer(int_slice.parse()?), int_slice.len()))
+}
+
+fn keyword_or_identifier_from_slice(s: &str) -> Result<(CToken, usize)> {
+    match keyword_from_slice(s) {
+        Some(x) => Ok(x),
+        None => identifier_token_from_slice(s),
+    }
+}
+
+fn keyword_from_slice(s: &str) -> Option<(CToken, usize)> {
+    // TODO Grab the list of keywords from somewhere and iterate them neatly!
+    for kw in vec!["int", "main"] {
+        if s.starts_with(kw) {
+            if let Some(c) = s.chars().nth(kw.len()) {
+                if c.is_whitespace() {
+                    return Some((CToken::Keyword(&s[0..kw.len()]), kw.len()));
+                }
+            }
+        }
+    }
+    None
+}
+
+fn identifier_token_from_slice(s: &str) -> Result<(CToken, usize)> {
+    let (_, kw_slice) = alphanumeric0::<&str, ()>(s)?; // Ignoring error type (second parameterized type)
+    match kw_slice.len() {
+        0 => bail!("Unexpected token:\n  {}", &s[0..min(20, s.len())]),
+        _ => Ok((CToken::Keyword(kw_slice), kw_slice.len())),
+    }
 }
 
 fn slurp_whitespace(s: &str) -> Result<(CToken, usize)> {
