6246a1b36ae25fce94bbb8e7f8cf23cea09e0c8f
Chris Tozer <chris.tozer@metaswitch.com>
2023-02-25 19:10:31 +0000

    Parse integer into token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use anyhow::{bail, Result};
+use nom::character::complete::digit0;
+// use nom::IResult;
+
+#[derive(Debug)]
+pub enum CToken<'a> {
+    Unknown(&'a str), // Something we haven't yet identified
+    Keyword(&'a str), // A keyword like 'int' or 'return'
+    OpenParen,        // (
+    CloseParen,       // )
+    OpenBrace,        // {
+    CloseBrace,       // }
+    SemiColon,        // ;
+    // Constants
+    Integer(i32), // A number
+    // Operators
+    // Whitespace
+    Whitespace,
+}
+
+// Entrypoint
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", token_from_slice("234"));
+}
+
+// Main tokenizer
+fn token_from_slice(data: &str) -> Result<(CToken, usize)> {
+    match data.chars().next() {
+        None => bail!("Unexpected EOF"),
+        Some(ch) => match ch {
+            ch if ch.is_digit(10) => int_token_from_slice(data),
+            _ => Ok((CToken::Unknown(data), data.len())),
+        },
+    }
+}
+
+// Specilised tokenizers
+fn int_token_from_slice(s: &str) -> Result<(CToken, usize)> {
+    let (_, int_slice) = digit0::<&str, ()>(s)?; // Ignoring error type (second parameterized type)
+    Ok((CToken::Integer(int_slice.parse()?), int_slice.len()))
 }
