137a4f4810a8b249f4810353342ad0900f47916d
Chris Tozer <chris.tozer@metaswitch.com>
2023-02-27 08:59:06 +0000

    Initial structure for simple return 2 parsing

--- src/parse.rs
+++ src/parse.rs
@@ -1,11 +1,33 @@
 use crate::lex::CToken;
 use anyhow::Result;
 
+// Simplified for only the use-cases we have at any point
 #[derive(Debug)]
-pub enum AST {
-    Program,
+pub enum AST<'a> {
+    Program(TopLevelConstructs<'a>),
+}
+
+#[derive(Debug)]
+pub enum TopLevelConstructs<'a> {
+    Function(&'a str, Statement),
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    Return(i32),
+}
+
+// Test piece for recursive Enum
+pub enum StatementRec<'a> {
+    IfStatement(&'a StatementRec<'a>),
+    ElseStatement(&'a StatementRec<'a>),
+    Comparison(&'a StatementRec<'a>),
 }
 
 pub fn parse_to_ast(tokens: Vec<CToken>) -> Result<AST> {
-    Ok(AST::Program)
+    // TODO - actually parse this from the tokens!
+    Ok(AST::Program(TopLevelConstructs::Function(
+        "main",
+        Statement::Return(2),
+    )))
 }
