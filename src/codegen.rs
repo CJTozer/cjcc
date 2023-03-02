fdcd511e5ba7ae1d55f2f166b91a02f4aa3ba8d5
Chris Tozer <chris.tozer@metaswitch.com>
2023-03-02 21:07:18 +0000

    Stage 4 complete

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,11 +1,16 @@
 use crate::parse::{BinaryOperator, Expression, Program, ReturnType, Statement, UnaryOperator};
 
+// Todo use an atomic or something
+static mut GLOBAL_ID: i32 = 0;
+
 // AST can currently only have one form: Program(Function("main", Integer, Return(12)))
 pub fn emit_code<'a>(prog: &'a Program<'a>) -> String {
     match prog {
         Program::Function(name, rtype, func) => codegen_function(name, rtype, func),
     }
 }
+
+// TODO pass around a mutable string reference rather than creating new ones and pushing those...
 
 fn codegen_function<'a>(name: &'a str, _rtype: &ReturnType, s: &'a Statement) -> String {
     let mut code = String::new();
@@ -100,26 +105,67 @@ fn codegen_binop<'a>(
             code.push_str(&codegen_inequality("sete", exp_a, exp_b));
         }
         BinaryOperator::NotEquality => {
-            // exp_a == exp_b
+            // exp_a != exp_b
             code.push_str(&codegen_inequality("setne", exp_a, exp_b));
         }
         BinaryOperator::GreaterThan => {
-            // exp_a == exp_b
+            // exp_a > exp_b
             code.push_str(&codegen_inequality("setg", exp_a, exp_b));
         }
         BinaryOperator::GreaterThanEq => {
-            // exp_a == exp_b
+            // exp_a >= exp_b
             code.push_str(&codegen_inequality("setge", exp_a, exp_b));
         }
         BinaryOperator::LessThan => {
-            // exp_a == exp_b
+            // exp_a < exp_b
             code.push_str(&codegen_inequality("setl", exp_a, exp_b));
         }
         BinaryOperator::LessThanEq => {
-            // exp_a == exp_b
+            // exp_a <= exp_b
             code.push_str(&codegen_inequality("setle", exp_a, exp_b));
         }
-        _ => todo!("Codegen for op {:?} not implemented", binop),
+        BinaryOperator::LogicalOr => {
+            // TODO get new labels which are unique
+            let this_id = get_next_id();
+            let clause2_label = format!("_clause{}", this_id);
+            let end_label = format!("_end{}", this_id);
+            // exp_a || exp_b - must short-circuit if exp_a is true.
+            code.push_str(&codegen_expression(exp_a));
+            // Check if exp_a is false
+            code.push_str("    cmpl $0, %eax\n");
+            // If false, jump to evaluating exp_b
+            code.push_str(&format!("    je {}\n", clause2_label));
+            // Otherwise short-circuit (evaluating to true)
+            code.push_str("    movl $1, %eax\n");
+            code.push_str(&format!("    jmp {}\n", end_label));
+            // Evaluate exp_b
+            code.push_str(&format!("    {}:\n", clause2_label));
+            code.push_str(&codegen_expression(exp_b));
+            code.push_str("    cmpl $0, %eax\n");
+            // Set %eax to 1 iff clause 2 was true
+            code.push_str("    movl $0, %eax\n");
+            code.push_str("    setne %al\n");
+            code.push_str(&format!("    {}:\n", end_label));
+        }
+        BinaryOperator::LogicalAnd => {
+            // TODO get new labels which are unique
+            let this_id = get_next_id();
+            let end_label = format!("_end{}", this_id);
+            // exp_a && exp_b - must short-circuit if exp_a is false.
+            code.push_str(&codegen_expression(exp_a));
+            // Check if exp_a is false
+            code.push_str("    cmpl $0, %eax\n");
+            // If false, jump to end
+            code.push_str(&format!("    je {}\n", end_label));
+            // Otherwise evaluate exp_b
+            code.push_str("    movl $1, %eax\n");
+            code.push_str(&codegen_expression(exp_b));
+            code.push_str("    cmpl $0, %eax\n");
+            // Set %eax to 1 iff clause 2 was true
+            code.push_str("    movl $0, %eax\n");
+            code.push_str("    setne %al\n");
+            code.push_str(&format!("    {}:\n", end_label));
+        }
     }
 
     code
@@ -140,4 +186,13 @@ fn codegen_inequality(set_instruction: &str, exp_a: &Expression, exp_b: &Express
     code.push_str(&format!("    {} %al\n", set_instruction));
 
     code
+}
+
+fn get_next_id() -> i32 {
+    let next_id;
+    unsafe {
+        next_id = GLOBAL_ID;
+        GLOBAL_ID += 1;
+    };
+    next_id
 }
